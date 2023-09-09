469e3201eaaec388f62eef4f357d27ab5630747f
Winniejoyful <114653625+Winniejoyful@users.noreply.github.com>
2023-09-09 11:44:19 +0100

    Add files via upload

---
+++ project.js
@@ -0,0 +1,36 @@
+//calling showtime function at every second
+setInterval (showtime,  1000);
+// defining showtime function
+function showtime( ){
+    // Getting current time and date
+    let time = new Date ();
+    let hour = time.getHours ();
+    let min = time.getMinutes ();
+    let sec = time.getSeconds();
+    am_pm = "AM";
+    // setting time for 12 hrs format
+    if (hour >= 12) {
+        if(hour > 12) hour -=12;
+        am_pm = "PM" ; 
+    }else if (hour== 0) {
+        hr = 12;
+        am_pm = "AM";
+    }
+    hour =
+    hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec ;
+    let currentTime =
+    hour +
+    ":" +
+    min +
+    ":" +
+    sec +
+    am_pm;
+    // displaying the time
+    document.getElementById(
+        "time"
+    ).innerHTML = currentTime;
+
+}
+showtime();
